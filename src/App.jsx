da1051d60ed61e235d9b19563aeae6eafb26dbed
rafaellindemann <rafaelld@gmail.com>
2025-04-02 21:15:14 -0300

    Lançado MVP do projeto

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { FaGithubSquare } from "react-icons/fa";
+function App() {
+
+  return (
+    <div className='container-app'>
+      <h1>Links do meu S2</h1>
+
+      <a href="https://github.com/rafaellindemann" className='links' target='_blank'>
+        <FaGithubSquare />
+        Github
+        <FaGithubSquare />
+      </a>
+
+    </div>
+  )
+}
+
+export default App
